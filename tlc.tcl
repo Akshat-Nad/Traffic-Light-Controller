6dcb4d530fbd28094c903237c276af0b3e576a07
Akshat <nadakshat5@gmail.com>
2025-08-26 18:57:21 +0530

    Update tlc.tcl

--- tlc.tcl
+++ tlc.tcl
@@ -8,40 +8,26 @@ read_physical -lef {../lef/gsclib045_tech.lef ../lef/gsclib045_macro.lef}
 read_netlist /home/nielit/aarushi/counter_design_database_45nm/physical_design/tlc_net.v -top traffic
 init_design
 
-
 connect_global_net VDD -type pg_pin -pin_base_name VDD -inst_base_name *
 connect_global_net VSS -type pg_pin -pin_base_name VSS -inst_base_name *
 
-#Create Floorplan
 create_floorplan -core_margins_by die -site CoreSite -core_density_size 1 0.4 2.5 2.5 2.5 2.5
 
-#Pin Assignment
 write_io_file -template tlc.io
 read_io_file tlc.io
 
-#Power Planning
 set_db add_rings_skip_shared_inner_ring none ; set_db add_rings_avoid_short 1 ; set_db add_rings_ignore_rows 0 ; set_db add_rings_extend_over_row 0
-
-#Add Rings
 add_rings -type core_rings -jog_distance 0.6 -threshold 0.6 -nets {VDD VSS} -follow core -layer {bottom Metal11 top Metal11 right Metal10 left Metal10} -width 0.7 -spacing .4 -offset 0.6
-
-#Add Stripes
 add_stripes -block_ring_top_layer_limit Metal11 -max_same_layer_jog_length 0.44 -pad_core_ring_bottom_layer_limit Metal9 -set_to_set_distance 5 -pad_core_ring_top_layer_limit Metal11 -spacing 0.4 -merge_stripes_value 0.6 -layer Metal10 -block_ring_bottom_layer_limit Metal9 -width 0.3 -nets {VDD VSS} 
 
-# Create Power Rails with Special Route
 route_special -connect core_pin -layer_change_range { Metal1(1) Metal11(11) } -block_pin_target nearest_target -core_pin_target first_after_row_end -allow_jogging 1 -crossover_via_layer_range { Metal1(1) Metal11(11) } -nets { VDD VSS } -allow_layer_change 1 -target_via_layer_range { Metal1(1) Metal11(11) }
-# Read the Scan DEF
 set_db reorder_scan_comp_logic true
-# Run Placement Optimization
 place_opt_design
-# Save the Database
 write_db placeOpt 
-# Create a Clock Tree Spec and run CTS
 create_clock_tree_spec
 ccopt_design 
-# Save the database
 write_db postCTSopt
-# Run Detail Routing
+
 set_db route_design_with_timing_driven 1
 set_db route_design_with_si_driven 1
 set_db design_top_routing_layer Metal11
@@ -52,10 +38,12 @@ set_db route_design_with_si_driven true
 route_design -global_detail
 reset_parasitics
 extract_rc
+
 write_parasitics -spef_file tlc.spef
 set_db timing_analysis_type ocv
 time_design -post_route -hold
-gui_select -point {-3.89300 9.17550}
+
+gui_select -point {-21.785 7.11650}
 set_db check_drc_disable_rules {}
 set_db check_drc_ndr_spacing auto
 set_db check_drc_check_only default
@@ -67,17 +55,17 @@ set_db check_drc_use_min_spacing_on_block_obs auto
 set_db check_drc_report traffic.drc.rpt
 set_db check_drc_limit 1000
 check_drc
+
 set_db check_drc_area {0 0 0 0}
 check_connectivity -type all -geometry_connect -error 1000 -warning 50
-#@ source power.tcl
-#@ Begin verbose source (pre): source power.tcl
+
 connect_global_net VDD -type pg_pin -pin VDD -inst *
 connect_global_net VSS -type pg_pin -pin VSS -inst *
 connect_global_net VDD -type tie_hi 
 connect_global_net VSS -type tie_lo 
 connect_global_net VDD -type tie_hi -pin VDD -inst *
 connect_global_net VSS -type tie_lo -pin VSS -inst *
-#@ End verbose source: power.tcl
+
 set_power_output_dir -reset
 set_power_output_dir ./run_tcl
 set_default_switching_activity -reset
